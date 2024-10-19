334a9e842287e7e189a2f0a7a5f3f60de3fbd0ae
mybllog <71851445+mybllog@users.noreply.github.com>
2024-10-19 14:05:32 +0100

    updated code

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -19,9 +19,9 @@ const Home = () => {
       </Helmet>
     <div className="relative w-full h-auto overflow-x-hidden">
       {/* Hero Section */}
-      <header className="relative w-full h-full ">
-        <img src={img1} alt="Innovative solutions backdrop" className=" w-full h-screen md:w-full md:h-full lg:w-full  lg:h-full object-cover" />
-        <div className="absolute top-12 xl:top-40 md:top-32 left-4 md:left-1/2 md:-translate-x-1/2 lg:left-20 lg:translate-x-0 text-white w-full px-4  lg:w-auto  lg:text-left">
+      <header className="relative w-full h-full bg-black inset-0 ">
+        <img src={img1} alt="Innovative solutions backdrop" className=" w-full relative h-screen md:w-full md:h-full lg:w-full  lg:h-full object-cover" />
+        <div className="absolute top-20 xl:top-40 md:top-32 left-4 md:left-1/2 md:-translate-x-1/2 lg:left-20 lg:translate-x-0 text-white w-full px-4  lg:w-auto  lg:text-left">
     <h1 className="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-bold font-newRomain leading-tight">
       Where Innovation <br /> Meets Boundless <br /> Possibilities
     </h1>
